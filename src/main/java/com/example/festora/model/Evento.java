349d9a2d340512fb107ba28612718d0fd1823c45
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-15 15:50:08 -0300

    Classe evento

---
+++ src/main/java/com/example/festora/model/Evento.java
@@ -0,0 +1,30 @@
+package com.example.festora.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "eventos")
+public class Evento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private String titulo;
+	
+	private String descricao;
+	
+	private LocalDateTime data;
+}
