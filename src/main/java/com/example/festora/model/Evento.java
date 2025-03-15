1a045f4791128fd46d13817c179a3fa4ff36b7bf
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-15 17:20:10 -0300

    Relação Evento e Usuario Organizador

--- src/main/java/com/example/festora/model/Evento.java
+++ src/main/java/com/example/festora/model/Evento.java
@@ -2,14 +2,18 @@ package com.example.festora.model;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.Collate;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -39,5 +43,9 @@ public class Evento {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "endereco_id", referencedColumnName = "id")
 	private Endereco endereco;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "usuario_id", referencedColumnName = "id")
+	private Usuario organizador;
 	
 }
--- src/main/java/com/example/festora/model/Usuario.java
+++ src/main/java/com/example/festora/model/Usuario.java
@@ -1,9 +1,14 @@
 package com.example.festora.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,4 +30,7 @@ public class Usuario {
 	private String email;
 	
 	private String senha;
+	
+	@OneToMany(mappedBy = "organizador", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Evento> eventos;
 }
