1563925d9e5b32aa71ac690858eae8c334212dca
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-15 16:05:18 -0300

    Classe Data Especial

---
+++ src/main/java/com/example/festora/model/DataEspecial.java
@@ -0,0 +1,26 @@
+package com.example.festora.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "datas_especiais")
+public class DataEspecial {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private LocalDateTime data;
+}
