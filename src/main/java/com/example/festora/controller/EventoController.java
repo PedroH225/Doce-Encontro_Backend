6620cbcbf1231b5b5e29a41f05a5bcf7b1e474c8
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-17 20:45:58 -0300

    Registro de eventos

--- src/main/java/com/example/festora/controller/EventoController.java
+++ src/main/java/com/example/festora/controller/EventoController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.festora.model.Evento;
+import com.example.festora.model.dtos.EventoRequestDTO;
+import com.example.festora.model.dtos.EventoResponseDTO;
 import com.example.festora.service.EventoService;
 
 
@@ -29,6 +33,11 @@ public class EventoController {
 	@GetMapping("/{id}")
 	public Evento obterPorId(@PathVariable String id) {
 		return eventoService.obterPorId(id);
+	}
+	
+	@PostMapping("/{organizadorId}")
+	public EventoResponseDTO registrarEvento(@PathVariable String organizadorId, @RequestBody EventoRequestDTO eventoDTO) {
+		return eventoService.registrarEvento(organizadorId, eventoDTO);
 	}
 }
 
--- src/main/java/com/example/festora/service/EventoService.java
+++ src/main/java/com/example/festora/service/EventoService.java
@@ -5,7 +5,12 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.festora.model.Endereco;
 import com.example.festora.model.Evento;
+import com.example.festora.model.Tipo;
+import com.example.festora.model.Usuario;
+import com.example.festora.model.dtos.EventoRequestDTO;
+import com.example.festora.model.dtos.EventoResponseDTO;
 import com.example.festora.repository.EventoRepository;
 
 @Service
@@ -15,8 +20,9 @@ public class EventoService {
 	
 	private UsuarioService usuarioService;
 
-	public EventoService(EventoRepository eventoRepository) {
+	public EventoService(EventoRepository eventoRepository, UsuarioService usuarioService) {
 		this.eventoRepository = eventoRepository;
+		this.usuarioService = usuarioService;
 	}
 	
 	public List<Evento> obterTodos() {
@@ -31,6 +37,20 @@ public class EventoService {
 		}
 		
 		return buscarEvento.get();
+	}
+	
+	public EventoResponseDTO registrarEvento(String organizadorId, EventoRequestDTO eventoDTO) {
+		Usuario buscarOrganizador = usuarioService.obterPorId(organizadorId);
+		
+		Endereco novoEndereco = 
+				new Endereco(null, eventoDTO.local(), eventoDTO.estado(), eventoDTO.cidade(), 
+						eventoDTO.rua(), eventoDTO.numero());
+		
+		Evento novoEvento = 
+				new Evento(null, eventoDTO.titulo(), eventoDTO.descricao(), Tipo.fromString(eventoDTO.tipo()), 
+				eventoDTO.data(), novoEndereco, buscarOrganizador, null, null);
+		
+		return new EventoResponseDTO(eventoRepository.save(novoEvento));
 	}
 	
 }
