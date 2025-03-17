43fb61ad049226790d2ee2396c0a50a9e7398ff6
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-17 18:38:16 -0300

    Inserts iniciais

---
+++ src/main/resources/sql/inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO `festora-db`.`usuarios` (id, nome, email, senha) VALUES
+('77eea251-9b21-42b1-b9cf-276035b66ed7', 'João Silva', 'joao@hotmail.com', 'senha123'),
+('37997bb5-8bba-4707-ae13-6d89b4cdf668', 'Maria Oliveira', 'maria@gmail.com', 'senha456'),
+('b634becf-47f9-4c99-a45e-f00f531e29bb', 'Carlos Souza', 'carlos@gmail.com', 'senha789');
