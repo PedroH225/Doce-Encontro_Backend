ffe10034373b7347327555dfac6dfd9ceb72717f
PedroH225 <pedrodguimaraes20@gmail.com>
2025-03-21 20:56:09 -0300

    Classe Requisito

---
+++ src/main/java/com/example/festora/model/Requisto.java
@@ -0,0 +1,43 @@
+package com.example.festora.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "requisitos")
+public class Requisto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private String titulo;
+
+	private String descricao;
+	
+	@ManyToOne
+	@JoinColumn(name = "evento_id", referencedColumnName = "id")
+	private Evento evento;
+	
+	@ManyToMany()
+	@JoinTable(name = "requisito_usuario",
+	joinColumns = @JoinColumn(name = "requisito_id"),
+	inverseJoinColumns = @JoinColumn(name = "usuario_id"))
+	private List<Usuario> responsaveis;
+
+}
