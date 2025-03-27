28dbca5c5451ef35ebf59ef4cf79ee3b2df3fd1e
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-27 16:36:47 -0300

    DTOs de autenticação

---
+++ src/main/java/com/example/festora/model/dtos/AuthetinticationDTO.java
@@ -0,0 +1,5 @@
+package com.example.festora.model.dtos;
+
+public record AuthetinticationDTO(String email, String password) {
+    
+}
---
+++ src/main/java/com/example/festora/model/dtos/LoginResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.festora.model.dtos;
+
+public record LoginResponseDTO(String token) {
+    
+}
