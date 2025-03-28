16b0b70de47e62f9bb571b24cdfb09761e3e7abf
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-28 18:41:47 -0300

    Senhas dos inserts criptografadas

--- src/main/resources/sql/inserts.sql
+++ src/main/resources/sql/inserts.sql
@@ -1,7 +1,7 @@
 INSERT INTO `festora-db`.`usuarios` (id, nome, email, senha) VALUES
-('77eea251-9b21-42b1-b9cf-276035b66ed7', 'João Silva', 'joao@hotmail.com', 'senha123'),
-('37997bb5-8bba-4707-ae13-6d89b4cdf668', 'Maria Oliveira', 'maria@gmail.com', 'senha456'),
-('b634becf-47f9-4c99-a45e-f00f531e29bb', 'Carlos Souza', 'carlos@gmail.com', 'senha789');
+('77eea251-9b21-42b1-b9cf-276035b66ed7', 'João Silva', 'joao@hotmail.com', '$2a$10$xkrk1iVWdWmUtvP0XjWzsu/KSKXMuor73lRdnpxvqBczTjVAOxXIG'),
+('37997bb5-8bba-4707-ae13-6d89b4cdf668', 'Maria Oliveira', 'maria@gmail.com', '$2a$10$hP/LHClj.PYP.i5rWRnWSOzeTkvfvUuTrA4smsEenHF5sPeTTK95W'),
+('b634becf-47f9-4c99-a45e-f00f531e29bb', 'Carlos Souza', 'carlos@gmail.com', '$2a$10$mCdgqORL0u5pLy9Y31MSEerJh8gzWnIzc9EyEkocGEWMi50NKR1jC');
 
 
 INSERT INTO `festora-db`.`eventos` (id, titulo, descricao, data, usuario_id) VALUES
