104580afd2ac513683bc96708393216267f41cf7
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-29 13:57:39 -0300

    Controlador global de exceções

---
+++ src/main/java/com/example/festora/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.festora.exception;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = ex.getBindingResult().getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(
+                FieldError::getField, 
+                FieldError::getDefaultMessage,
+                (mensagem1, mensagem2) -> mensagem1
+            ));
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+}
