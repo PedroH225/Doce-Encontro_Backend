021eba77858eec213b489b2d1bca0c954ca8fd8b
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-29 13:58:51 -0300

    Validação registro usuário

--- src/main/java/com/example/festora/model/dtos/RegisterDTO.java
+++ src/main/java/com/example/festora/model/dtos/RegisterDTO.java
@@ -1,7 +1,25 @@
 package com.example.festora.model.dtos;
 
-import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
-public record RegisterDTO(@NotNull String nome, @NotNull String email, @NotNull String senha) {
+import com.example.festora.annotation.SenhaForte;
+import com.example.festora.utils.MensagemErro;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterDTO(
+		@NotBlank(message = MensagemErro.OBRIGATORIO) @Length(min = 5, message = MensagemErro.MIN) 
+		@Length(max = 20, message = MensagemErro.MAX)
+		String nome, 
+		
+		@NotBlank(message = MensagemErro.OBRIGATORIO) @Email(message = MensagemErro.INVALIDO) 
+		String email, 
+		
+		@NotBlank(message = MensagemErro.OBRIGATORIO) @SenhaForte 
+		@Length(max = 20, message = MensagemErro.MAX)
+		String senha
+		
+		) {
     
 }
