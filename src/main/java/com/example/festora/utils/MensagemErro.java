eb70d941e119e241d76dced67636ae300629c64c
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-29 13:57:13 -0300

    Mensagens de erro

---
+++ src/main/java/com/example/festora/utils/MensagemErro.java
@@ -0,0 +1,14 @@
+package com.example.festora.utils;
+
+public class MensagemErro {
+
+	public static final String OBRIGATORIO = "Campo obrigatório.";
+	
+	public static final String INVALIDO = "Campo inválido.";
+	
+	public static final String MIN = "O campo deve conter no mínimo 5 caracteres.";
+	
+	public static final String MAX = "O campo deve conter no máximo 20 caracteres.";
+	
+	private MensagemErro() {}
+}
