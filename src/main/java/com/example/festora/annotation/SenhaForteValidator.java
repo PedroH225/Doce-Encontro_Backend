cabb6f04615da9190324eb327825ee16c2ba5a33
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-03-29 13:58:37 -0300

    Anotação senha forte

---
+++ src/main/java/com/example/festora/annotation/SenhaForteValidator.java
@@ -0,0 +1,14 @@
+package com.example.festora.annotation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class SenhaForteValidator implements ConstraintValidator<SenhaForte, String> {
+
+    private static final String REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+    @Override
+    public boolean isValid(String senha, ConstraintValidatorContext context) {
+        return senha != null && senha.matches(REGEX);
+    }
+}
