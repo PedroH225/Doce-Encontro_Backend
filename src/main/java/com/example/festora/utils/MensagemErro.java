13d724d839d7ea46c9b587e4091b36c710a9f9ba
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-04-09 14:23:51 -0300

    Escolher quantidade de caracteres

--- src/main/java/com/example/festora/utils/MensagemErro.java
+++ src/main/java/com/example/festora/utils/MensagemErro.java
@@ -6,9 +6,9 @@ public class MensagemErro {
 	
 	public static final String INVALIDO = "Campo inválido.";
 	
-	public static final String MIN = "O campo deve conter no mínimo 5 caracteres.";
+	public static final String MIN = "O campo deve conter no mínimo {min} caracteres.";
 	
-	public static final String MAX = "O campo deve conter no máximo 20 caracteres.";
+    public static final String MAX = "O campo deve conter no máximo {max} caracteres.";
 	
 	private MensagemErro() {}
 }
