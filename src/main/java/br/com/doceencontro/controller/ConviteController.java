ebb0e4610a069286731c515f8a336583a06fe3e9
PedroH225 <pedrodguimaraes20@gmail.com>
2025-04-14 22:12:35 -0300

    Convidar participantes

--- src/main/java/br/com/doceencontro/controller/ConviteController.java
+++ src/main/java/br/com/doceencontro/controller/ConviteController.java
@@ -1,8 +1,14 @@
 package br.com.doceencontro.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.doceencontro.model.dtos.ConviteDTO;
 import br.com.doceencontro.service.ConviteService;
 
 @RestController
@@ -15,7 +21,10 @@ public class ConviteController {
 		this.conviteService = conviteService;
 	}
 	
-	
+	@PostMapping("/{eventoId}")
+	public ConviteDTO convidarParticipantes(@PathVariable String eventoId, @RequestBody List<String> usuariosIds) {
+		return conviteService.convidar(usuariosIds, eventoId);
+	}
 }
 
 
--- src/main/java/br/com/doceencontro/service/ConviteService.java
+++ src/main/java/br/com/doceencontro/service/ConviteService.java
@@ -1,44 +1,69 @@
 package br.com.doceencontro.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import br.com.doceencontro.model.Convite;
+import br.com.doceencontro.model.Evento;
+import br.com.doceencontro.model.Usuario;
+import br.com.doceencontro.model.dtos.ConviteDTO;
 import br.com.doceencontro.repository.ConviteRepository;
-import br.com.doceencontro.repository.UsuarioRepository;
+import br.com.doceencontro.repository.EventoRepository;
 
 @Service
 public class ConviteService {
 
 	private ConviteRepository conviteRepository;
-	
+
+	private EventoRepository eventoRepository;
+
 	private UsuarioService usuarioService;
-	
+
 	private EventoService eventoService;
 
-	public ConviteService(ConviteRepository conviteRepository) {
+	public ConviteService(ConviteRepository conviteRepository, EventoService eventoService,
+			UsuarioService usuarioService, EventoRepository eventoRepository) {
 		this.conviteRepository = conviteRepository;
+		this.usuarioService = usuarioService;
+		this.eventoService = eventoService;
+		this.eventoRepository = eventoRepository;
 	}
-	
+
 	public List<Convite> findAll() {
 		return conviteRepository.findAll();
 	}
-	
+
 	public Convite findById(String id) {
 		Optional<Convite> buscarConvite = conviteRepository.findById(id);
-		
+
 		if (buscarConvite.isEmpty()) {
 			throw new RuntimeException("Convite não encontrado.");
 		}
-		
+
 		return buscarConvite.get();
 	}
+
+	private ConviteDTO converterDto(Convite convite) {
+		return new ConviteDTO(convite);
+	}
+
+	public ConviteDTO convidar(List<String> usuariosIds, String eventoId) {
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+
+		Evento evento = eventoService.findById(eventoId);
+
+		usuariosIds.forEach(id -> {
+			usuarios.add(usuarioService.findById(id));
+		});
+		
+		evento.getConvite().enviarConvite(usuarios);
+
+		Evento eventodb = this.eventoRepository.save(evento);
+		
+		return converterDto(eventodb.getConvite());
+	}
 }
-
-
-
-
-
-
