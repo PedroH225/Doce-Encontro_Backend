1087ff861e5ab397ac06730017e9f4a05d7a49c4
PedroH225 <pedrodguimaraes20@gmail.com>
2025-04-17 16:48:05 -0300

    EventoUtils

---
+++ src/main/java/br/com/doceencontro/utils/EventoUtils.java
@@ -0,0 +1,53 @@
+package br.com.doceencontro.utils;
+
+import java.util.Optional;
+
+import br.com.doceencontro.exception.exceptions.JaParticipandoException;
+import br.com.doceencontro.exception.exceptions.NotParticipandoException;
+import br.com.doceencontro.model.Evento;
+import br.com.doceencontro.model.Usuario;
+
+public class EventoUtils {
+
+	public static boolean isParticipando(Evento evento, String usuarioId) {
+		Optional<Usuario> buscarUsuario = evento.getParticipantes().stream()
+				.filter(p -> p.getId().equals(usuarioId))
+				.findFirst();
+		
+		if (buscarUsuario.isPresent()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void garantirParticipacao(Evento evento, String usuarioId) {
+		Optional<Usuario> buscarUsuario = evento.getParticipantes().stream()
+				.filter(p -> p.getId().equals(usuarioId))
+				.findFirst();
+		
+		if (buscarUsuario.isEmpty()) {
+			throw new NotParticipandoException();
+		}
+	}
+	
+	public static void garantirNaoParticipacao(Evento evento, String usuarioId) {
+		Optional<Usuario> buscarUsuario = evento.getParticipantes().stream()
+				.filter(p -> p.getId().equals(usuarioId))
+				.findFirst();
+		
+		if (buscarUsuario.isPresent()) {
+			throw new JaParticipandoException();
+		}
+
+	}
+	
+	public static boolean verificarAutor(String usuarioId, Evento evento) {		
+		if (evento.getOrganizador().getId().equals(usuarioId)) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	
+}
